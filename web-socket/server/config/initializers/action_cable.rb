99f1eae3350adc9794b59c15e2e7c57599c88c6d
smokgier <johnypolaszek@gmail.com>
2026-02-17 13:47:31 +0100

    Działanie na przeglądarce oraz na innych komputerach

--- web-socket/server/config/initializers/action_cable.rb
+++ web-socket/server/config/initializers/action_cable.rb
@@ -1,2 +1,10 @@
 Rails.application.config.action_cable.disable_request_forgery_protection = true
-Rails.application.config.action_cable.allowed_request_origins = [/http:\/\/localhost:\d+/, /file:\/\//, 'null']
+Rails.application.config.action_cable.allowed_request_origins = [
+  /http:\/\/localhost:\d+/,
+  /http:\/\/127\.0\.0\.1:\d+/,
+  /http:\/\/192\.168\.\d+\.\d+:\d+/,
+  /http:\/\/10\.\d+\.\d+\.\d+:\d+/,
+  /http:\/\/172\.(1[6-9]|2[0-9]|3[0-1])\.\d+\.\d+:\d+/,
+  /file:\/\//,
+  'null'
+]
